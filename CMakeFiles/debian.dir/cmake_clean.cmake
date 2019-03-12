25d2a448b610f1ac5eba7decaeab15b6a0a05461
Aditya Vamsikrishna Mandalika <adityavk711@gmail.com>
2019-03-11 19:55:20 -0700

    Revert "remove builds"
    This reverts commit 102fcdb8e842bb3873c5446c3a1e085387081468.

---
+++ CMakeFiles/debian.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/debian"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/debian.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
