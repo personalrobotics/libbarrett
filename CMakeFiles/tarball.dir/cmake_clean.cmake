52b1f5012dca4721865a07b289e99da05dfdb2b8
Aditya Vamsikrishna Mandalika <adityavk711@gmail.com>
2019-03-11 19:55:26 -0700

    Revert "Revert "remove builds""
    This reverts commit 25d2a448b610f1ac5eba7decaeab15b6a0a05461.

--- CMakeFiles/tarball.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/tarball"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/tarball.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
