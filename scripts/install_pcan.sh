62e0c790c815bdcb3a54079d22a0bbbcefe3a4ad
Markus Grotz <grotz@uw.edu>
2024-06-07 16:19:42 -0700

    add simple check if driver is already downloaded

--- scripts/install_pcan.sh
+++ scripts/install_pcan.sh
@@ -7,9 +7,15 @@
 if [ "$DISTRIB_RELEASE" = "22.04" ]; then
 	# Install PCAN driver
 	mydir=$(pwd)
+	if [ ! -d /root/peak-linux-driver-8.11.0 ]; then
 	cd && wget https://www.peak-system.com/fileadmin/media/linux/files/peak-linux-driver-8.11.0.tar.gz
 	tar xf peak-linux-driver-8.11.0.tar.gz
-	cd peak-linux-driver-8.11.0 && make -j$(nproc) -C driver netdev
+	else
+		echo "peak-linux-driver-8.11.0" already downloaded
+	fi
+	exit 
+	cd peak-linux-driver-8.11.0 
+	make -j$(nproc) -C driver netdev
 	sudo make -C driver install
 	cd $mydir
 else
